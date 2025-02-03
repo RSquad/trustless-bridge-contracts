8650a58a20804ff6cf865bc1cad5e46aec23efdc
NikitaM <21960067+Keshoid@users.noreply.github.com>
2025-02-03 19:07:11 +0300

    chore: add FeeCollector to measure gas consuming

---
+++ jest-env-vars.js
@@ -0,0 +1,1 @@
+process.env.PRINT_FEES = true;
--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  setupFiles: ["./jest-env-vars.js"],
 };
 
 export default config;
